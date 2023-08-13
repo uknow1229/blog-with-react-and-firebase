3d75378a5db613d3effda85225502871ad4d5b9f
saya <uknow.love1229@gmail.com>
2023-08-13 22:20:47 +0900

    add: home-page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきたいです。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@ShinCode</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
