134843f9c5470db26fa2f23d0f8117a6d3978bea
saya <uknow.love1229@gmail.com>
2023-08-13 18:13:29 +0900

    add: logout-function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,16 @@
 import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
   const loginInWithGoogle = () => {
     //Googleでログイン
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem('isAuth', true);
       setIsAuth(true);
+      navigate('/');
     });
   };
   return (
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,24 @@
+import { signInWithPopup, signOut } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
-  return <div>Logout</div>;
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    //ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/login');
+    });
+  };
+  return (
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>Googleでログアウト</button>
+    </div>
+  );
 };
 
 export default Logout;
