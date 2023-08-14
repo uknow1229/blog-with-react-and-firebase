49a4b74ce6cf6dd87d48b86bd6bf159a4d598ca2
saya <uknow.love1229@gmail.com>
2023-08-14 09:41:45 +0900

    fix: reload-bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
             <div className="postTextContainer">{post.postsText}</div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              {post.author.id === auth.currentUser.uid && (
+              {post.author.id === auth.currentUser?.uid && (
                 <button onClick={() => handleDelete(post.id)}>削除</button>
               )}
             </div>
